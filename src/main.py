594216c875c4b2f7f66ae2fe1388b810396a1988
ruilagarto-dev <ruimanuelagarto@gmail.com>
2025-06-08 11:44:29 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from modules.view import TubeTuneApp
+from modules.controller import TubeTuneController
+from modules.model import YTDownloadManager
 
+
+if __name__ == "__main__":
+    model = YTDownloadManager()
+    app = TubeTuneApp(400, 250)
+    controller = TubeTuneController(app, model)
+    
+    app.run()
